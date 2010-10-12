d79e59f9925d76be4d49d4f8e25be779c35ad551
shilpa singh <shilpa.singh@samsung.com>
2010-10-12 20:34:43 +0900

    [ELM_PASSWORD_SHOW_LAST_CHARACTER]- variable added.

--- etc/profile.d/elm.sh
+++ etc/profile.d/elm.sh
@@ -5,3 +5,4 @@ export ELM_FPS=6000
 export ELM_INPUT_PANEL=1
 export EDJE_MULTISENSE_UI_MODULE=slp_sound_haptic
 export ELM_MODULES="ctxpopup_copypasteUI>entry/api:decarta>map/api"
+export ELM_PASSWORD_SHOW_LAST_CHARACTER=1
