ab9d920978b429cce5e7f22183bf85a9d736da0d
Daniel Juyung Seo <juyung.seo@samsung.com>
2010-10-12 09:32:44 +0900

    Fixed bugs.

--- etc/profile.d/elm.sh
+++ etc/profile.d/elm.sh
@@ -4,4 +4,4 @@ export ELM_BRING_IN_SCROLL_FRICTION=0.5
 export ELM_FPS=6000
 export ELM_INPUT_PANEL=1
 export EDJE_MULTISENSE_UI_MODULE=slp_sound_haptic
-export ELM_MODULES=ctxpopup_copypasteUI>entry/api:decarta>map/api
+export ELM_MODULES="ctxpopup_copypasteUI>entry/api:decarta>map/api"
