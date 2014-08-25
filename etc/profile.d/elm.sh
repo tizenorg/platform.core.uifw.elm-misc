6e9f21b880f9c26944ad26aee54c55e303770192
Philippe Coval <philippe.coval@open.eurogiciel.org>
2014-08-25 14:57:34 +0200

    tizen: set system wide ELM_THEME
    Change-Id: If1e9514c0d1ddd848c1bc8779b07814d1eb4bbad
    Signed-off-by: Philippe Coval <philippe.coval@open.eurogiciel.org>

--- etc/profile.d/elm.sh
+++ etc/profile.d/elm.sh
@@ -2,3 +2,5 @@ export ELM_PROFILE="mobile"
 export ELM_MODULES="ctxpopup_copypasteUI>entry/api:datetime_input_ctxpopup>datetime/api"
 # efl-1.8
 export ELM_MODULES="$ELM_MODULES:prefs>prefs_iface"
+
+export ELM_THEME="tizen-HD-light"
